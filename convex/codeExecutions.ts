386df418e2e85f270c33b5173dba49b77e14750f
Ayushmaan218 <121386512+Ayushmaan218@users.noreply.github.com>
2025-10-28 22:11:05 +0530

    minor bug

--- convex/codeExecutions.ts
+++ convex/codeExecutions.ts
@@ -1,5 +1,5 @@
 import { ConvexError, v } from "convex/values";
-import { mutation, query } from "./_generated/server";
+import { mutation, query } from "./_generated/server.js";
 import { paginationOptsValidator } from "convex/server";
 
 export const saveExecution = mutation({
